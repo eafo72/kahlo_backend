6ec54e9387a1fb1c558ff888b6ecfb752d85c2cf
eafo72 <eafo72@gmail.com>
2025-09-10 12:30:18 -0600

    agregar timestamp al codigo de boleto

--- src/routes/photos.js
+++ src/routes/photos.js
@@ -10,7 +10,7 @@ const db = require('../config/db')
 // CREAR UN USUARIO JWT
 app.post('/start', async (req, res) => {
 	try {
-		let { id_usuario, nombre, correo, telefono } = req.body 
+		let { id_usuario, nombre, correo, telefono, codigo } = req.body 
 
 		let errors = Array();
 
@@ -25,6 +25,9 @@ app.post('/start', async (req, res) => {
 		}
 		if (!telefono) {
 			telefono = null;
+		}
+		if (!codigo) {
+			errors.push({ msg: "El campo código debe de contener un valor" });
 		}
 
 		if (errors.length >= 1) {
@@ -44,18 +47,21 @@ app.post('/start', async (req, res) => {
 		let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
 		let time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 		let fecha = date + ' ' + time;
+		
+			
+		codigo = codigo + "_" + fecha;
 
 
 		query = `INSERT INTO fotografias
-					(id_usuario, nombre, correo, telefono, created_at, updated_at) 
+					(id_usuario, nombre, correo, telefono, codigo, created_at, updated_at) 
 					VALUES 
-					('${id_usuario}', '${nombre}', '${correo}', '${telefono}', '${fecha}', '${fecha}')`;
+					('${id_usuario}', '${nombre}', '${correo}', '${telefono}', '${codigo}', '${fecha}', '${fecha}')`;
 
 
 		let result = await db.pool.query(query);
 		result = result[0];
 		
-		res.status(200).json({ error: false, id: result.insertId })
+		res.status(200).json({ error: false, id: codigo })
 
 
 	} catch (error) {
