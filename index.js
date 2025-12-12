9751a850ce21611439d186649da391d2da4148ee
Alex <alex@agencianuba.com>
2025-12-12 15:18:40 -0600

    ajuste stripe

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const server = http.createServer(app);
